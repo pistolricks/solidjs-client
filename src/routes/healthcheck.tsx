9dd9ff3a8f8dc4d7fb9d1ce34e47524dffd58509
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-01-16 16:25:23 -0800

    all info in healthcheck

--- src/routes/healthcheck.tsx
+++ src/routes/healthcheck.tsx
@@ -17,11 +17,9 @@ export default function Healthcheck() {
     return (
         <main class="text-center mx-auto text-gray-700 p-4">
             <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Health Check</h1>
-
             <p class="mt-8 uppercase">
                 {check()?.status} - {check()?.system_info?.environment} - {check()?.system_info?.version}
              </p>
-            
         </main>
     );
 }
