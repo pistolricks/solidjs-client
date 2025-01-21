5e9c244276464545487a95fda71e6a882b214ae6
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-01-20 22:25:12 -0800

    fixed create movie layout

--- src/routes/movies/create.tsx
+++ src/routes/movies/create.tsx
@@ -8,11 +8,9 @@ import CreateMovieForm from "~/components/movies/forms/create-movie-form";
 const Create: Component<VoidComponent> = () => {
 
     return (
-        <AppLayout>
             <FormLayout>
                 <CreateMovieForm/>
             </FormLayout>
-        </AppLayout>
     );
 };
 
