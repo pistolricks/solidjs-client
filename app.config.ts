23d438213e2e3b9ef3a089190c10eb95023e60ea
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-01-29 05:01:12 -0800

    maybe i should have changed branches

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,19 @@
 import { defineConfig } from "@solidjs/start/config";
+import solidPlugin from 'vite-plugin-solid';
+import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        // vite options
+        plugins: [
+            tailwindcss(),
+            solidPlugin({})
+        ],
+        server: {
+            port: 3000,
+        },
+        build: {
+            target: 'esnext',
+        },
+    },
+});
--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -6,14 +6,17 @@ export default createHandler(() => (
   <StartServer
     document={({ assets, children, scripts }) => (
       <html class="h-full bg-gray-app scroll-smooth" lang="en">
-        <head>
-          <meta charset="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
+      <head>
+          <meta charset="utf-8"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1"/>
+          <link rel="icon" href="/favicon.ico"/>
+          {/*
+          <link href="/dist/styles.css" rel="stylesheet"/>
+          */}
           {assets}
-        </head>
-        <body class="h-full">
-          <div id="app">{children}</div>
+      </head>
+      <body class="h-full">
+      <div id="app">{children}</div>
           {scripts}
         </body>
       </html>
