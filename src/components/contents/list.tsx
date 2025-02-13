5ce5b342aa724142f6faf5bacd9b79d2a77f690f
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-02-13 14:12:03 -0800

    contents gallery almost done

--- src/components/contents/list.tsx
+++ src/components/contents/list.tsx
@@ -1,7 +1,10 @@
-import {Component, createEffect, createSignal, For} from "solid-js";
-import {ContentsData} from "~/lib/store";
+import {Component, createEffect, createSelector, createSignal, For} from "solid-js";
+import {CONTENT, ContentsData} from "~/lib/store";
 import {ByteWithLocale} from "~/components/ui/format-byte";
 import {ResponsiveDialog} from "~/components/ui/dialogs/responsive-dialog";
+import DrawerPrimitive from "@corvu/drawer";
+import Dialog from "@corvu/dialog";
+import {Button} from "~/components/ui/button";
 
 type PROPS = {
     contents: ContentsData | undefined;
@@ -18,32 +21,74 @@ const ContentsList: Component<PROPS> = props => {
         console.log(getContents())
     })
 
-    return (
-        <ul role="list"
-            class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8 container">
-            <For each={getContents()}>
-                {(content, i) => (
-                    <li class="relative">
-                        <div
-                            class="group overflow-hidden rounded-lg focus-within:ring-2 focus-within:ring-blue-5 focus-within:ring-offset-2 focus-within:ring-offset-gray-1">
-                            <img
-                                src={`http://localhost:4000/${content.src}`}
-                                alt="" class="pointer-events-none aspect-[1/1] w-full object-cover group-hover:opacity-75"/>
-                            <button type="button" class="absolute inset-0 focus:outline-none">
-                                <span class="sr-only">{content.original}</span>
-                            </button>
-                        </div>
-                        <p class="pointer-events-none mt-3 block truncate text-sm font-medium text-gray-11">{content.original}</p>
-                        <div class={'flex justify-between items-center text-accent'}>
-                            <span class={'text-xs py-1'}>{content.type}</span>
-                            <ByteWithLocale value={content.size}/>
-                        </div>
-                    </li>
-                )}
-            </For>
+    const [getSelectedId, setSelectedId] = createSignal<string>()
+    const [getSelected, setSelected] = createSignal<CONTENT>()
+    const isSelected = createSelector(getSelectedId)
 
-            <ResponsiveDialog/>
-        </ul>
+    const handler = (data: CONTENT, event: Event) => {
+        console.log("Data:", data, "Event:", event);
+        setSelectedId(data.id)
+        if (isSelected(data.id)) {
+            setSelected(data)
+        }
+
+    };
+
+
+    return (
+        <ResponsiveDialog>
+
+            <DrawerPrimitive.Content contextId={'dd1'}>
+                <img
+                    src={`http://localhost:4000/${getSelected()?.src}`}
+                    alt=""
+                    class="pointer-events-none h-full w-full object-contain group-hover:opacity-75"/>
+                <DrawerPrimitive.Description contextId={'dd1'}>
+                    Make changes to your profile here. Click save when you're done.
+                </DrawerPrimitive.Description>
+
+                <DrawerPrimitive.Close contextId={'dd1'} as={Button<"button">} variant="outline">
+                    Cancel
+                </DrawerPrimitive.Close>
+            </DrawerPrimitive.Content>
+
+            <Dialog.Content contextId={'dd1'} class="sm:max-w-[425px]">
+
+                <img
+                    src={`http://localhost:4000/${getSelected()?.src}`}
+                    alt=""
+                    class="pointer-events-none h-full w-full object-contain group-hover:opacity-75"/>
+
+                <Dialog.Description contextId={'dd1'}>
+                    Make changes to your profile here. Click save when you're done.
+                </Dialog.Description>
+            </Dialog.Content>
+
+            <ul role="list"
+                class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8 container">
+                <For each={getContents()}>
+                    {(content, i) => (
+                        <Dialog.Trigger as={'li'} class={'relative'} contextId={'dd1'} onClick={[handler, content]} classList={{active: isSelected(content.id)}}>
+                            <div
+                                class="group overflow-hidden rounded-lg focus-within:ring-2 focus-within:ring-blue-5 focus-within:ring-offset-2 focus-within:ring-offset-gray-1">
+                                <img
+                                    src={`http://localhost:4000/${content.src}`}
+                                    alt=""
+                                    class="pointer-events-none aspect-[1/1] w-full object-cover group-hover:opacity-75"/>
+                                <button type="button" class="absolute inset-0 focus:outline-none">
+                                    <span class="sr-only">{content.original}</span>
+                                </button>
+                            </div>
+                            <p class="pointer-events-none mt-3 block truncate text-sm font-medium text-gray-11">{content.original}</p>
+                            <div class={'flex justify-between items-center text-accent'}>
+                                <span class={'text-xs py-1'}>{content.type}</span>
+                                <ByteWithLocale value={content.size}/>
+                            </div>
+                        </Dialog.Trigger>
+                    )}
+                </For>
+            </ul>
+        </ResponsiveDialog>
     );
 };
 
--- src/components/ui/dialogs/responsive-dialog.tsx
+++ src/components/ui/dialogs/responsive-dialog.tsx
@@ -1,91 +1,34 @@
-import type { ComponentProps } from "solid-js"
-import { createSignal, onMount, Show } from "solid-js"
+import {createSignal, JSXElement, onMount, Show} from "solid-js"
+import {Button} from "~/components/ui/button"
 
-import { cn } from "~/lib/utils"
-import { Button } from "~/components/ui/button"
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger
-} from "~/components/ui/dialog"
-import {
-    Drawer,
-    DrawerClose,
-    DrawerContent,
-    DrawerDescription,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerTitle,
-    DrawerTrigger
-} from "~/components/ui/drawer"
-import { TextField, TextFieldInput, TextFieldLabel } from "~/components/ui/text-field"
 import DrawerPrimitive from "@corvu/drawer";
+import Dialog from "@corvu/dialog";
 
-export function ResponsiveDialog() {
+type PROPS = {
+    children?: JSXElement
+}
+
+export function ResponsiveDialog(props: PROPS) {
     const [open, setOpen] = createSignal(false)
     const [isDesktop, setIsDesktop] = createSignal(false)
+
+    const children = () => props.children;
 
     onMount(() => {
         setIsDesktop(window.innerWidth >= 768)
     })
 
     const MobileDialog = () => (
-        <DrawerPrimitive contextId={'md1'} dialogId="responsive-drawer-1" open={open()} onOpenChange={setOpen}>
-            <DrawerPrimitive.Trigger contextId={'md1'} as={Button<"button">} variant="outline">
-                Edit Profile
-            </DrawerPrimitive.Trigger>
-            <DrawerPrimitive.Content contextId={'md1'}>
-                <DrawerHeader class="text-left">
-                    <DrawerTitle contextId={'md1'} >Edit profile</DrawerTitle>
-                    <DrawerPrimitive.Description contextId={'md1'}>
-                        Make changes to your profile here. Click save when you're done.
-                    </DrawerPrimitive.Description>
-                </DrawerHeader>
-                <ProfileForm class="px-4" />
-                <DrawerFooter class="pt-2">
-                    <DrawerClose contextId={'md1'}  as={Button<"button">} variant="outline">
-                        Cancel
-                    </DrawerClose>
-                </DrawerFooter>
-            </DrawerPrimitive.Content>
+        <DrawerPrimitive contextId={'dd1'} open={open()} onOpenChange={setOpen}>
+            {children()}
         </DrawerPrimitive>
     )
 
     return (
-        <Show when={isDesktop()} fallback={<MobileDialog />}>
-            <Dialog id="responsive-dialog-1" open={open()} onOpenChange={setOpen}>
-                <DialogTrigger as={Button} variant="outline">
-                    Edit Profile
-                </DialogTrigger>
-                <DialogContent class="sm:max-w-[425px]">
-                    <DialogHeader>
-                        <DialogTitle>Edit profile</DialogTitle>
-                        <DialogDescription>
-                            Make changes to your profile here. Click save when you're done.
-                        </DialogDescription>
-                    </DialogHeader>
-                    <ProfileForm />
-                </DialogContent>
+        <Show when={isDesktop()} fallback={<MobileDialog/>}>
+            <Dialog contextId={'dd1'} open={open()} onOpenChange={setOpen}>
+                {children()}
             </Dialog>
         </Show>
-    )
-}
-
-function ProfileForm(props: ComponentProps<"form">) {
-    return (
-        <form class={cn("grid items-start gap-4", props.class)}>
-            <TextField class="grid gap-2">
-                <TextFieldLabel>Email</TextFieldLabel>
-                <TextFieldInput placeholder="shadcn@example.com" type="email" />
-            </TextField>
-            <TextField class="grid gap-2">
-                <TextFieldLabel>Username</TextFieldLabel>
-                <TextFieldInput placeholder="@shadcn" type="text" />
-            </TextField>
-            <Button type="submit">Save changes</Button>
-        </form>
     )
 }
